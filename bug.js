229fb48812b76f030cccedfae11f2f5117241114
BugHunter-X <askzenai@gmail.com>
2025-01-14 06:30:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Some Tailwind CSS classes are not applied correctly.
+<div class="bg-red-500 hover:bg-blue-700 p-4">
+  Hover over me!
+</div>
+```
+The above code snippet is expected to change the background color to blue on hover, but it doesn't. This is a common issue when using Tailwind CSS with JavaScript frameworks or when there are conflicts with other CSS rules.
